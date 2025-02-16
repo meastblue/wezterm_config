132ceae98f9fbb94205187da5a0070eac427abcd
meastblue <amalou.massinissa@gmail.com>
2025-02-16 14:10:49 +0100

    bind keymap

--- wezterm.lua
+++ wezterm.lua
@@ -6,6 +6,8 @@
 --- wezterm.lua - personal configuration
 
 local wezterm = require 'wezterm'
+local action = wezterm.action
+local act = wezterm.action
 
 local config = {}
 if wezterm.config_builder then
@@ -30,7 +32,30 @@ config = {
     color_scheme = "Tokyo Night (Gogh)",
     window_background_opacity = 0.9,
     text_background_opacity = 0.9,
+
+    -- keys
+    keys = {
+        { mods = "OPT",     key = "LeftArrow",  action = action.SendKey({ mods = "ALT", key = "b" }) },
+        { mods = "OPT",     key = "RightArrow", action = action.SendKey({ mods = "ALT", key = "f" }) },
+        { mods = "CMD",     key = "LeftArrow",  action = action.SendKey({ mods = "CTRL", key = "a" }) },
+        { mods = "CMD",     key = "RightArrow", action = action.SendKey({ mods = "CTRL", key = "e" }) },
+        { mods = "CMD",     key = "Backspace",  action = action.SendKey({ mods = "CTRL", key = "u" }) },
+        { mods = 'CMD|ALT', key = 'LeftArrow',  action = act.ActivateTabRelative(-1) },
+        { mods = 'CMD|ALT', key = 'RightArrow', action = act.ActivateTabRelative(1) },
+    }
 }
 
+local bind = function(mods, key, action)
+    table.insert(config.keys, {
+        key = key,
+        mods = mods,
+        action = action,
+    })
+end
+
+bind('CMD', 'k', act.Multiple {
+    act.ClearScrollback 'ScrollbackAndViewport',
+    act.SendKey { key = 'L', mods = 'CTRL' },
+})
 
 return config
